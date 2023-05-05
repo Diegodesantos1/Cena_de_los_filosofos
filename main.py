20826a1afe1f639f282332abcda777e6a26cc7c9
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-05-05 02:26:49 +0200

    main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from interfaz import Ventana
+from filosofos import N, filosofo
 
+if __name__ == "__main__":
+    V = Ventana()
+
+    lista = []
+    for i in range(N):
+        lista.append(filosofo(V))
+
+    for f in lista:
+        f.start()
+    V.run()
+    for f in lista:
+        f.join()
